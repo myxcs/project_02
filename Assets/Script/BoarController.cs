5ad69c5acd693c817ed80fabd9df65e246ed7204
Nguyen Quang Thai <118356147+myxcs@users.noreply.github.com>
2024-04-06 23:53:25 +0700

    smol update

--- Assets/Script/BoarController.cs
+++ Assets/Script/BoarController.cs
@@ -4,43 +4,82 @@ using UnityEngine;
 
 public class BoarController : MonoBehaviour
 {
-    public SpriteRenderer sr;
-    public Animator anim;
-
-    public Transform boar;
-    public Transform posA;
-    public Transform posB;
-
-    bool isFaceRight = true;
+     public SpriteRenderer sprite;
+     public Animator animator;
+     public Rigidbody2D rb;
+     public Transform boar;
+    // public Transform posA;
+    // public Transform posB;
+     public bool isFaceRight = false;
     float speedMove = 3f;
+    float timer = 0f;
+    float timer2 = 0f;
+    float timeBetweenMove = 5f;
+    float timeRest = 7f;
+
+    private void Start()
+    {
+         rb = GetComponent<Rigidbody2D>();
+         boar = GetComponent<Transform>();
+         animator = GetComponent<Animator>();
+         animator.SetTrigger("idle");
+         sprite = GetComponent<SpriteRenderer>();
+         sprite.flipX = true;
+         isFaceRight = true;
+
+    }
 
     private void Update()
     {
-        if (isFaceRight) //nó đang đi sang phải -> posB
-        {
-            if (Vector2.Distance(boar.position, posB.position) > 0.1f)
-            {
-                boar.position = Vector3.MoveTowards(boar.position, posB.position, speedMove * Time.deltaTime);
-                anim.Play("Boar_Running");
-            }
-            else
-            {
-                isFaceRight = false;
-                sr.flipX = false;
-            }
-        }
-        else //nó đang đi trang trái -> posA
-        {
-            if (Vector2.Distance(boar.position, posA.position) > 0.1f)
-            {
-                boar.position = Vector3.MoveTowards(boar.position, posA.position, speedMove * Time.deltaTime);
-                anim.Play("Boar_Running");
-            }
-            else
-            {
-                isFaceRight = true;
-                sr.flipX = true;
-            }
-        }
+        GoRight();
     }
+    private void GoRight()
+    {
+              timer += Time.deltaTime;
+              if(isFaceRight)
+              {
+                  boar.position = Vector3.MoveTowards(boar.position, new Vector3(boar.position.x + 5f, boar.position.y, 0), speedMove * Time.deltaTime);
+                  animator.SetTrigger("running");
+                  if(timer > timeBetweenMove)
+                  {
+                      
+                        animator.SetTrigger("idle");
+                        rb.bodyType = RigidbodyType2D.Static;
+                        timer2 += Time.deltaTime;
+                      if(timer2 > timeRest)
+                      {
+                          
+                          timer = 0;
+                          timer2 = 0;
+                          rb.bodyType = RigidbodyType2D.Dynamic;
+                          sprite.flipX = false;
+                          isFaceRight = false;
+                      }
+                  }
+                  
+              }
+              else
+              {
+                  boar.position = Vector3.MoveTowards(boar.position, new Vector3(boar.position.x + -5f, boar.position.y, 0), speedMove * Time.deltaTime);
+                  animator.SetTrigger("running");
+                  if(timer > timeBetweenMove)
+                  {
+                      animator.SetTrigger("idle");
+                      rb.bodyType = RigidbodyType2D.Static;
+                      timer2 += Time.deltaTime;
+                      if(timer2 > timeRest)
+                      {
+                        
+                        timer = 0;
+                        timer2 = 0;
+                        rb.bodyType = RigidbodyType2D.Dynamic;
+                        sprite.flipX = true;
+                        isFaceRight = true;
+                      }
+                  }
+              }
+          
+          
+    } 
 }
+
